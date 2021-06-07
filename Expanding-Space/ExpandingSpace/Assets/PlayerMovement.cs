4aca3e2830f4fe64beee9e2c23744186e8a89d71
PiterGroot <70685433+PiterGroot@users.noreply.github.com>
2021-06-07 13:06:26 +0200

    Hub nu in dezelfde scene

---
+++ Expanding-Space/ExpandingSpace/Assets/PlayerMovement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private GameObject SpaceShip;
+    [SerializeField] private WaveSpawner spawner;
+    [SerializeField]private GameObject HubFoundation;
+    public bool canMove;
+    [SerializeField] private Rigidbody2D rb;
+    public float speed = 10;
+    void Update()
+    {
+        if (canMove)
+        {
+            //rechts
+            if (Input.GetKey("a"))
+            {
+                rb.velocity = new Vector3(-speed, 0, 0);
+            }
+            //links
+            if (Input.GetKey("d"))
+            {
+                rb.velocity = new Vector3(speed, 0, 0);
+            }
+
+            if (Input.GetKeyUp("w"))
+            {
+                rb.velocity = new Vector3(0, 0, 0);
+            }
+
+            if (Input.GetKeyUp("a"))
+            {
+                rb.velocity = new Vector3(0, 0, 0);
+            }
+
+            if (Input.GetKeyUp("s"))
+            {
+                rb.velocity = new Vector3(0, 0, 0);
+            }
+
+            if (Input.GetKeyUp("d"))
+            {
+                rb.velocity = new Vector3(0, 0, 0);
+            }
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Exit" && Input.GetKey(KeyCode.F)){
+            HubFoundation.SetActive(false);
+            spawner.waitForPlayerChoice = true;
+            SpaceShip.SetActive(true);
+        }
+    }
+}
