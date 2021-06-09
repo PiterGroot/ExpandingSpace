1d29c3a22bf814727ae23d67a6124da6f9270811
Michelle <Minoord18@outlook.com>
2021-06-09 15:08:19 +0200

    Commit to pull

---
+++ Expanding-Space/ExpandingSpace/Assets/NewProject/Scipt/Enemies/SchootingEnemie.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SchootingEnemie : MonoBehaviour
+{
+    int timer; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
