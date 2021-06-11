23a9e11186ef386e8e89435e247cfd5f0d568756
Michelle <Minoord18@outlook.com>
2021-06-11 09:59:46 +0200

    Shooting Enemie gemaakt

--- Expanding-Space/ExpandingSpace/Assets/NewProject/Scipt/Enemies/SchootingEnemie.cs
+++ Expanding-Space/ExpandingSpace/Assets/NewProject/Scipt/Enemies/SchootingEnemie.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class SchootingEnemie : MonoBehaviour
 {
-    int timer; 
+    int timer;
+    int ShootTimer;
+    public GameObject Bullet;
+    public Vector2 Spawnpoint;
     // Start is called before the first frame update
     void Start()
     {
-        timer = 10;
+        timer = 1000;
+        ShootTimer = 600;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Spawnpoint = this.gameObject.transform.position;
+        timer -= 1;
+        ShootTimer -= 1;
+        if(timer <= 0)
+        {
+            if (ShootTimer <= 0)
+            {
+                ShootTimer = 600;
+                Instantiate(Bullet, Spawnpoint, Quaternion.identity);
+            }
+        }
+
     }
 }
---
+++ Expanding-Space/ExpandingSpace/Assets/NewProject/Scipt/Other/Bullets/EnemieBullet.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemieBullet : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector2(-5, 0) * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            collision.gameObject.GetComponent<MoveObj>().KillSelf();
+            Destroy(gameObject);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "DespawnBullet")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
