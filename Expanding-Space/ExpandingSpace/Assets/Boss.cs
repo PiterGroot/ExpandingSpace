48acc2df57aead150691fc4d690374528dd33525
PiterGroot <70685433+PiterGroot@users.noreply.github.com>
2021-06-16 14:17:23 +0200

    Verder aan boss

--- Expanding-Space/ExpandingSpace/Assets/Boss.cs
+++ Expanding-Space/ExpandingSpace/Assets/Boss.cs
@@ -14,8 +14,9 @@ public class Boss : MonoBehaviour
     [SerializeField]private TriggerDialogue BossDialogue;
     [SerializeField]private Transform GunPos;
     [SerializeField]private GameObject bullet;
-    [SerializeField, Range(.05f, .5f)]private float ShootSpeed;
+    [SerializeField, Range(0, .5f)]private float ShootSpeed;
     [SerializeField]private int BulletCount;
+    [SerializeField] private int BulletMultiplier;
     private void Start() {
         StartCoroutine(TriggerBossDialogue());
     }
@@ -30,8 +31,15 @@ public class Boss : MonoBehaviour
         gunAnim.SetBool("CanShoot", true);
         for (int i = 0; i < amountofbullets; i++){
             yield return new WaitForSeconds(ShootSpeed);
-            Instantiate(bullet, GunPos.position, GunPos.rotation);   
+           StartCoroutine(ShootBullet(BulletMultiplier)); 
         }
         gunAnim.SetBool("CanShoot", false);
+    }
+
+    private IEnumerator ShootBullet(int count){
+        for (int i = 0; i < count; i++){
+            yield return new WaitForSeconds(ShootSpeed + 0.1f);
+            Instantiate(bullet, GunPos.position, GunPos.rotation);
+        }
     }
 }
